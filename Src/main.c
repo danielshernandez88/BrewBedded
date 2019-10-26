b428181402281ebc6c6b78d08b008fcf62656002
Daniel Sanchez <daniel.shernandez88@gmail.com>
2019-10-26 12:45:07 -0500

    PWM configuration

--- Src/main.c
+++ Src/main.c
@@ -76,7 +76,9 @@ uint16_t temp;
 char buffer[20];
 ADC_HandleTypeDef hadc1;
 uint32_t valueAdc[3], adc_buffer[3], read_values[3];
+GPIO_InitTypeDef GPIO_InitStruct;
 
+uint16_t valuepwm = 20;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -106,6 +108,18 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 			valueAdc[i] = adc_buffer[i];
 		}
 	}
+}
+
+void user_pwm_setvalue(uint16_t value)
+{
+    TIM_OC_InitTypeDef sConfigOC;
+
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = value;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
 }
 /* USER CODE END 0 */
 
@@ -169,6 +183,21 @@ int main(void)
   }
 
   HAL_ADC_Start_DMA(&hadc1, adc_buffer, 3);
+
+
+
+  GPIO_InitStruct.Pin = PWM_Peltier2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(PWM_Peltier2_GPIO_Port, &GPIO_InitStruct); // Port A
+
+  GPIO_InitStruct.Pin = PWM_Peltier1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(PWM_Peltier1_GPIO_Port, &GPIO_InitStruct); // Port B
+
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
@@ -387,9 +416,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
+  htim2.Init.Prescaler = 14000;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 0;
+  htim2.Init.Period = 200-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
@@ -587,6 +616,8 @@ void StartTask10ms(void const * argument)
 
 		if(cnt100ms > 100)
 		{
+
+			user_pwm_setvalue(valuepwm);
 			if (TM_DS18B20_Is(DS_ROM))
 			{
 				/* Everything is done */
@@ -631,6 +662,7 @@ void StartTask10ms(void const * argument)
 			cnt100ms++;
 		}
 	}
+
   /* USER CODE END StartTask10ms */
 }
 
